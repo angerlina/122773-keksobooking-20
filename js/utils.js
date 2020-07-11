c0fa22c39427f38af608903cf6f2b1db9a721a3b
anger_lina <iratnovskaya@gmail.com>
2020-07-11 18:07:52 +0400

    17. Личный проект: надо подкачаться (часть 1)

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,7 @@
 window.utils = (function () {
 
   var getRandomInt = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.round((max - min) * Math.random());
   };
 
   var formatPixelValueToInt = function (pixels) {
